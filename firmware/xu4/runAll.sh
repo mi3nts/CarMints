ed508c18c08fc1841a42d7f2529d6e1cee59f84c
john-waczak <john.louis.waczak@gmail.com>
2019-11-15 15:03:01 -0600

    fixed spelling error

--- firmware/xu4/runAll.sh
+++ firmware/xu4/runAll.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #
-sleep 60
+# note, rs232monitor.py uses command line flag to specify
+# rs232 device to monitor.
+#
+#
+sleep 15
 python3 rs232monitor.py 0 &
 sleep 5
 python3 rs232monitor.py 1 &
 sleep 5
 python3 rs232monitor.py 2 &
+sleep 5
+python3 licorMonitor.py & 
