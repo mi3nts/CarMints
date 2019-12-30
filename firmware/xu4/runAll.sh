2f8a5c0d9e60e113ab6f549dce77883034d58e1e
john-waczak <john.louis.waczak@gmail.com>
2019-12-30 09:18:42 -0600

    added check to see if process exists so script can be rerun multiple times

--- firmware/xu4/runAll.sh
+++ firmware/xu4/runAll.sh
@@ -5,12 +5,57 @@
 #
 #
 sleep 120
-python3 rs232monitor.py 0 &
-sleep 5
-python3 rs232monitor.py 1 &
-sleep 5
-python3 rs232monitor.py 2 &
-sleep 5
-python3 licorMonitor.py &
+if -z ps aux | grep 'rs232monitor.py 0'
+then
+    exit
+else
+    python3 rs232monitor.py 0 &
+    sleep 5
+fi
+
+if -z ps aux | grep 'rs232monitor.py 1'
+then
+    exit
+else
+    python3 rs232monitor.py 1 &
+    sleep 5
+fi
+
+if -z ps aux | grep 'rs232monitor.py 2'
+then
+    exit
+else
+    python3 rs232monitor.py 2 &
+    sleep 5
+fi
+
+if -z ps aux | grep 'licorMonitor.py'
+then
+    exit
+else
+    python3 licorMonitor.py &
+    sleep 5
+fi
+
+if -z ps aux | grep 'GPSReader.py'
+then
+    exit
+else
+    python3 GPSReader.py &
+    sleep 5
+fi
+
+
+# Old Version
+# python3 rs232monitor.py 0 &
+# sleep 5
+# python3 rs232monitor.py 1 &
+# sleep 5
+# python3 rs232monitor.py 2 &
+# sleep 5
+# python3 licorMonitor.py &
 # sleep 5
 # python3 GPSReader.py
+
+
+
